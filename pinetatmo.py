91652cb653195cf48b2bc0b622dd8e359169ea29
Simon Powell <simon@powellmail.co.uk>
2017-06-04 20:59:47 +0100

    Added forecast

--- pinetatmo.py
+++ pinetatmo.py
@@ -26,7 +26,9 @@ while True:
     try:
         weatherData = lnetatmo.WeatherStationData(netatmo_authorization)
         latest_data = weatherData.lastData()
+        forecast = apixu_client.getForecastWeather(q=apixu_location, days=2)
     except:
+        print 'Unable to update data, skipping'
         pass
 
     data_time = datetime.datetime.fromtimestamp(latest_data['Outdoor']['When']).strftime('%Y-%m-%d %H:%M')
@@ -40,23 +42,25 @@ while True:
     else:
         trend_symbol = 'a'
     
-    forecast = apixu_client.getForecastWeather(q=apixu_location, days=2)
-    if datetime.datetime.now().hour > 16:
+    if datetime.datetime.now().hour > 15:
         forecast_day = 1
         forecast_title = "Tomorrow"
     else:
         forecast_day = 0
         forecast_title = "Today"
     forecast_high = '{: 6.1f}'.format(forecast['forecast']['forecastday'][forecast_day]['day']['maxtemp_c'])
-    
+    forecast_icon = forecast['forecast']['forecastday'][forecast_day]['day']['condition']['icon']
+    forecast_text = forecast['forecast']['forecastday'][forecast_day]['day']['condition']['text']
+
     screen.AddText('Outdoor', 5, 3, 20, Id='OutTitle', font_path=font)
     screen.AddText(out_temp + u"\u00b0", 5, 10, 70, Id="OutTemp", font_path=font)
     screen.AddText(trend_symbol, 170, 30, 50, font_path=arrow_font)
     screen.AddText('Humidity ' + str(out_humidity) + '%', 5, 86, 11, Id='Humidity', font_path=font)
     screen.AddText('Bedroom', 5, 103, 15, Id='InTitle', font_path=font)
-    screen.AddText(bedroom_temp + u"\u00b0", 5, 117, 35, Id='InTemp', font_path=font)
-    screen.AddText(forecast_title, 130, 103, 15, Id='FcTitle', font_path=font)
-    screen.AddText(forecast_high + u"\u00b0", 130, 117, 35, Id='FcTemp', font_path=font)
+    screen.AddText(bedroom_temp + u"\u00b0", 5, 115, 35, Id='InTemp', font_path=font)
+    screen.AddText(forecast_title, 130, 93, 15, Id='FcTitle', font_path=font)
+    screen.AddText(forecast_high + u"\u00b0", 130, 105, 35, Id='FcTemp', font_path=font)
+    screen.AddText(forecast_text, 130, 143, 11, Id='FcText', font_path=font)
     screen.AddText(data_time, 10, 164, 10, Id='When', font_path=font)
     screen.WriteAll()
 
